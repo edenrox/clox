4eb8c365a3c094c109f0a10570b54f5b3e3b9d67
Ian Hopkins <ian@lucidhelix.com>
2020-04-05 20:38:38 -0400

    Control flow: if, while, for working.
    At end of Chapter 23. Jumping Back and Forth.
    
    Note: fixed some bugs in the compiler as well.

--- vm.c
+++ vm.c
@@ -77,6 +77,7 @@ static void concatenate() {
 static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define READ_SHORT() (vm.ip += 2, (uint16_t)((vm.ip[-2] << 8) | vm.ip[-1]))
 #define READ_STRING() AS_STRING(READ_CONSTANT())
 
 #define BINARY_OP(valueType, op) \
@@ -170,6 +171,7 @@ static InterpretResult run() {
         break;
       case OP_LESS:
         BINARY_OP(BOOL_VAL, <);
+        break;
       case OP_ADD:
         if (IS_STRING(peek(0)) && IS_STRING(peek(1))) {
           concatenate();
@@ -206,6 +208,23 @@ static InterpretResult run() {
         printf("\n");
         break;
       }
+      case OP_JUMP: {
+        uint16_t offset = READ_SHORT();
+        vm.ip += offset;
+        break;
+      }
+      case OP_JUMP_IF_FALSE: {
+        uint16_t offset = READ_SHORT();
+        if (isFalsey(peek(0))) {
+          vm.ip += offset;
+        }
+        break;
+      }
+      case OP_LOOP: {
+        uint16_t offset = READ_SHORT();
+        vm.ip -= offset;
+        break;
+      }
       case OP_RETURN:
       	// Exit interpreter.
         return INTERPRET_OK;
@@ -214,6 +233,7 @@ static InterpretResult run() {
 
 #undef BINARY_OP
 #undef READ_BYTE
+#undef READ_SHORT
 #undef READ_CONSTANT
 #undef READ_STRING
 }
